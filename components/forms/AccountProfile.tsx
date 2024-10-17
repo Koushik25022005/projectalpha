f8f89f22f8a388c58348c8fcc9fdd442bf8779c1
maitymohar <maitymohar2005@gmail.com>
2024-10-17 20:12:40 +0530

    Updated Onboarding

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,94 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { userValidation } from "@/lib/validations/user"
+import {zodResolver} from "@hookform/resolvers/zod"
+import Image from "next/image"
+import { useForm } from "react-hook-form";
+import { z } from "zod"
+
+interface Props {
+    user: {
+      id: string;
+      objectId: string;
+      username: string;
+      name: string;
+      bio: string;
+      image: string;
+    };
+    btnTitle: string;
+  }
+
+const AccoutnProfile = ({user, btnTitle}: Props)=>{
+    const form = useForm({
+        resolver: zodResolver(userValidation),
+        defaultValues:{
+            profile_photo:'',
+            name: '',
+            username: '',
+            bio: '',
+        }
+
+    })
+    function onSubmit(values: z.infer<typeof userValidation>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+      }
+    return (
+        <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} 
+        className="flex flex-col justify-start gap-10"
+        >
+          <FormField
+            control={form.control}
+            name="profile_photo"
+            render={({ field }) => (
+              <FormItem className="flex items-center gap-4">
+                <FormLabel className="account-form_image-label">
+                    { field.value? (
+                        <Image
+                        src={field.value}
+                        alt="profile photo"
+                        width={96}
+                        height={96}
+                        priority
+                        className="rounded-full object-contain"
+                        />
+                    ): (
+                        <Image
+                        src="/assets/profile.svg"
+                        alt="profile photo"
+                        width={96}
+                        height={96}
+                        priority
+                        className="rounded-full object-contain"
+                        />
+                    )}
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    )
+}
+
+export default AccoutnProfile
